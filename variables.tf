c76fef2e0b9ff1e9a3c680178d4d6b8b2e632de2
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2016-08-31 14:22:35 +0100

    Basic cleaning

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "environment" {
 
 variable "ha-nat_subnets_cidrs" {
   type        = "list"
-  default     = ["10.222.1.0/24", "10.222.2.0/24", "10.222.3.0/24"]
+  default     = []
   description = "A list of CIDR blocks to create NAT Gateways in, iterating through available availability zones"
 }
 
@@ -24,9 +24,6 @@ variable "peering_connections" {
 variable "peer_matrix" {
   type    = "list"
   default = []
-  #   "0123456789012,vpc-01234567890123456,10.99.0.0/16",
-  #   "2109876543210,vpc-65432109876543210,172.16.0.0/16"
-  # ]
   description = "List of peering partner tuples (['peer_aws_account_id,peer_vpc_id,peer_cidr_block']) with which to create vpc peering connections"
 }
 
